e40ecd638eb6c5060486a2aa49fb58a9acb7d5a0
bancow-khy <bancow-khy@bancow-khyui-MacBookPro.local>
2021-11-03 19:46:15 +0900

    9장

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 import SassComponent from "./SassComponent";
+import StyledComponent from "./StyledComponents";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
